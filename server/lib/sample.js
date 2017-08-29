94adec4c267248102c0df05c9d00ac9da8876a04
alexsicart <alex@sharge.io>
2017-08-29 11:26:10 +0200

    fixing

--- server/lib/sample.js
+++ server/lib/sample.js
@@ -1,24 +1,3 @@
-/*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-/**
-* Close the bidding for a vehicle listing and choose the
-* highest bid that is over the asking price
-* @param {org.acme.energy.CloseBidding} closeBidding - the closeBidding transaction
-* @transaction
-*/
-
 function closeBidding(closeBidding) {
   var listing = closeBidding.listing;
   if (listing.state !== 'AVAILABLE') {
